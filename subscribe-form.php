94fe9eff64d69930c6e57645b9f3ea7339be1958
Greenbox.id <info@gb.co.id>
2014-04-11 01:19:52 +0700

    Update subscribe-form.php

--- subscribe-form.php
+++ subscribe-form.php
@@ -1,27 +1,19 @@
 <?php
-
-
-
-
 $email=$_POST['email'];
 
-
-$to='yourname@mail.com';
+$to='aank84@gmail.com';
 
 $headers = 'From: '.$email."\r\n" .
 	'Reply-To: '.$email."\r\n" .
 	'X-Mailer: PHP/' . phpversion();
 	
-$subject = 'Subscribe request for Applechic';
-
+$subject = 'Subscribe request for Greenhouse Project Site';
 
 $body.='Email: '.$email."\n";
 
-	
 if(mail($to, $subject, $body, $headers)) {
 	die('Subscription complete.');
 } else {
 	die('Error: Mail failed');
 }
-
 ?>
